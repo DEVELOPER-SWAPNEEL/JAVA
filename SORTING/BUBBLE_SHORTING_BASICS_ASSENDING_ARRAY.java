03e58b8fdba9ebeb091f9021f80f8c676c530e03
SWAPNEEL PUROHIT <rsnikupurohit17@gmail.com>
2024-05-21 09:03:02 +0530

    Sorting_(21/05/2024)

---
+++ SORTING/BUBBLE_SHORTING_BASICS_ASSENDING_ARRAY.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class BUBBLE_SORTING_BASICS_ASSENDING_ARRAY {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER OF ELEMENTS PRESENT IN THE ARRAY YOU WANT TO SORT: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print("ENTER THE " + (i + 1) + "TH ELEMENT IN THE ARRAY: ");
+            arr[i] = sc.nextInt();
+        }
+
+        // Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // Print sorted array
+        System.out.println("SORTED ARRAY IN ASCENDING ORDER: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+
+        // Close the scanner
+        sc.close();
+    }
+}
